b7df7138d42a5c40a82560187460ef8878457c2a
Cezary <156573752+Cezary-Sylwestrzak@users.noreply.github.com>
2024-02-26 18:50:12 +0100

    "Dodanie do programu funkcji interakcji z użytkownikiem oraz na podstawie tej interakcji budowa funkcji matematycznej programu."

--- Program3/Program.cs
+++ Program3/Program.cs
@@ -6,10 +6,28 @@ namespace Program3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Math.Pow(5, 2) + Math.Pow(10, 2));
-            Console.WriteLine(Math.Sqrt(125));
+            Console.WriteLine("Witaj w aplikacji do obliczania długości przekątnej prostokąta.");
+            Console.WriteLine("Aby obliczyć przekątną potrzebujemy danych na temat długości boków.");
 
+            Console.WriteLine(string.Empty);
 
+            Console.WriteLine("Podaj długość pierwszego boku.");
+            string valueNumberOne = Console.ReadLine();
+            int valueNumberOneInt = int.Parse(valueNumberOne);
+
+            Console.WriteLine(string.Empty);
+
+            Console.WriteLine("Podaj długość drugiego boku.");
+            string valueNumberTwo = Console.ReadLine();
+            int valueNumberTwoInt = int.Parse(valueNumberTwo);
+
+            Console.WriteLine(string.Empty);
+
+            Console.WriteLine("Przekątna prostokąta wynosi:");
+            int power = 2;
+            Console.WriteLine(Math.Sqrt(Math.Pow(valueNumberOneInt, power) + Math.Pow(valueNumberTwoInt, power)));
+
+           
             
         }
         
